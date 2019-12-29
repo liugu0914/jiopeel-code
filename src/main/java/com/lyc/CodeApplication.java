7b757c9e7fb7b28a20247398c096ce2d3999259f
lyc <liugu0914@sina.com>
2019-12-30 00:56:02 +0800

    code auto build

--- src/main/java/com/lyc/CodeApplication.java
+++ src/main/java/com/lyc/CodeApplication.java
@@ -1,7 +1,9 @@
 package com.lyc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -9,12 +11,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @Author: lyc
  * @Date: 2019/9/29 17:07
  */
+@Slf4j
 @EnableTransactionManagement
 @SpringBootApplication
 public class CodeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CodeApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(CodeApplication.class, args);
+        String port = applicationContext.getEnvironment().getProperty("server.port");
+        log.info("jiopeel started at http://localhost:"+port);
     }
 
 }
